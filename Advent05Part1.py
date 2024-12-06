3e59db6a1a416e4e883743cb13ab6229530b6376
[Michael Bekhit] <[michael.bekhit@gmail.com]>
2024-12-06 14:11:35 +0000

    Advent05Part1 Incomplete

--- Advent05Part1.py
+++ Advent05Part1.py
@@ -1,9 +1,17 @@
-# input contains pairs of numbers. Each pair creates a rule whereby the first must precede the second.
-# the second part of the input is a sequence and the sequence must be checked against all the rules
+# input has 2 parts. 1st contains pairs of numbers. Each pair creates a rule whereby the first must precede the second.
+# the second part of the input is a sequence:list[int] and the sequence must be checked against all the rules
 # whichever sequences remain must be arranged such that the middle sequence can be selected. The output should be the total of
 # the values contained in that middle sequence.
 import re
 
+def multiple_index(value, sequence:list):
+    matching_indices = []
+    
+    if str(value) in sequence:
+        for i in range(len(sequence)):
+            if str(value)==sequence[i]:
+                matching_indices.append(i)
+    return matching_indices
 rules = []
 sequences = []
 with open("input05.txt","r") as textfile:
@@ -23,32 +31,34 @@ for rule in rules:
 
 # print(rules_tuples)
 test_sequence = sequences[0]
+test_sequence2 = ["57", "60","57", "60", "60"]
 # ['57,47,82,32,18']
 accepted_sequences = []
 rules = []
-accepted = True
-
-# process for single sequence
 
 
-for first,second in rules_tuples:
 
-    if second == "57":
-        print(first)
-    if first in test_sequence and second in test_sequence:
-        if test_sequence.index(first) > test_sequence.index(second):
-            accepted = False
-    
-if accepted:
-    accepted_sequences.append(test_sequence)
-
-print(accepted_sequences)
-
+for sequence in sequences:
+    # print(sequence)
+    accepted = True
+    for first,second in rules_tuples:
 
         
+        if first in sequence and second in sequence:
+            first_position = sequence.index(first)
+            # print(sequence.index(first))
+            indices_of_second = multiple_index(second), sequence
+            # print(multiple_index(second,sequence))
+            if first_position > min(indices_of_second):
+                accepted = False
+                
+    
+    if accepted:
+        accepted_sequences.append(sequence)
 
 
-
+# print(len(sequences),len(accepted_sequences))
+print(len(accepted_sequences))
         
 
 
