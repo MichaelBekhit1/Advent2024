69b3ec59910d674429e9d43215a996844bdb70c5
[Michael Bekhit] <[michael.bekhit@gmail.com]>
2024-12-14 16:16:04 +0000

    Advent06Part1Save

--- Advent06Part1.py
+++ Advent06Part1.py
@@ -1,53 +1,47 @@
 import itertools
+import re
 class Guard:
-    def __init__(self, grid, current_coords:tuple, next, direction="^"):
+    def __init__(self, grid, current_coords:tuple, target_coords:tuple, directions:list[tuple]):
         self.current_coords = current_coords
-        self.direction = direction
-        self.next = next
         self.grid = grid
-        next = (current_coords[0], current_coords[(1+1)])
+        self.target_coords = target_coords
+        self.directions = directions
+        target_coords = ((current_coords[0]+1), current_coords[1])
+        
 
+        # merge direction and next tightly-coupled
+        # do without symbols
+        # remove repitition
 
-
-    def turn_90_at_obstacle(self, obstacle):
-        if self.next == obstacle:
-
-            if self.direction == "^":
-                self.next = (0, 1 )
-            # -->    
-            elif self.direction == ">":
-                self.next = (-1, 0)
-            # ¬    
-            elif self.direction == "¬":
-                self.next = (1, 0)
-            # <--   
-            elif self.direction == "<":
-                self.next = (-1,0)
-            # ^    
-
-
-    def proceed(self, obstacle):
-        while #in bounds
+    def proceed(self):
+        
+        # print(self.current_coords)
+        # print(self.grid[self.current_coords[0]][self.current_coords[1]])
+        # print(self.grid[self.target_coords[0]][self.target_coords[1]])
+        directions_index = 0
+        #print(self.target_coords[0] + self.directions[directions_index][0])
+        while 0 < self.current_coords[0] < cols and 0 < self.current_coords[1] < rows:
+            print("x")
+            # runs 8 times
             
-            if self.next == obstacle:
-                self.turn_90_at_obstacle(obstacle)
+            if self.grid[self.target_coords[0]][self.target_coords[1]] == "#":
+                print("y")
+                # loops once more then drops
+                self.target_coords=((self.target_coords[0] + self.directions[directions_index][0]),(self.target_coords[1] + self.directions[directions_index][1]))
+                
+                if directions_index < len(directions):
+                    directions_index +=1
+                else:
+                    directions_index = 0
+                
             else:
-                self.grid[self.current_coords] = "X"
+                
+                self.grid[self.current_coords[0]][self.current_coords[1]] = "X"
 
-                self.current_coords = ((self.current_coords[0]+self.next[0]), (self.current_coords[1]+ self.next[1]))
+                self.current_coords = ((self.target_coords[0]), (self.target_coords[1]))
+                self.target_coords = ((self.target_coords[0]+self.directions[directions_index][0]), (self.target_coords[1]+self.directions[directions_index][1]))
 
-                if self.direction == "^":
-                    self.next = (-1, 0 )
-                # ^    
-                elif self.direction == ">":
-                    self.next = (0, 1)
-                # -->    
-                elif self.direction == "¬":
-                    self.next = (-1, 0)
-                # ¬   
-                elif self.direction == "<":
-                    self.next = (0,-1)
-                # <--
+                
         return self.grid
 
 
@@ -56,8 +50,28 @@ with open("dummy_input06.txt","r") as textfile:
 
 cols = len(lines)
 rows = len(lines[0])
+# directions = ["up", "right", "down", "left"]
+directions = [(-1, 0), (0, 1), (1,0), (0,-1)]
+# directions_cycle = itertools.cycle(directions)
+directions_index = 0
+# counter = itertools.count()
+array = lines
+array_split = []
+for line in array:
+    line = "!".join(line)
+    line = line.split("!")
+    array_split.append(line)
+    
+# print(array_split[6][4])
+for col, row in enumerate(array):
+    x = re.search("[\^]", row)
+    if x:
+        current_coords = (col, x.start())
+        # print(current_coords)
+# get current coords ##########################
+#print(current_coords)
 
-counter = itertools.count()
-array = zip(counter,lines)
-current_coords = # find "^" index
-print(list(array))
+target_coords = (7 , 4)
+guard = Guard(array_split,current_coords,target_coords, directions)
+for col, row in enumerate(guard.proceed()):
+    print(col,row)
