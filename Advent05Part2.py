3056649fb6d5f66384b2b1c7a98f89bbe56ddf30
[Michael Bekhit] <[michael.bekhit@gmail.com]>
2024-12-21 10:44:35 +0000

    Advent05Part2Incomplete

--- Advent05Part2.py
+++ Advent05Part2.py
@@ -13,55 +13,72 @@ with open("input05.txt","r") as textfile:
         regex = re.findall("(\d\d)\|(\d\d)", line)
         if regex:
            rules.append(regex[0])
+           # this just skips the empty first line
     for line in lines[len(rules)+1::]:
         sequences.append(line.strip().split(","))
         
                     
-# for each sequence, every value appearing in the second position in the rules list should appear after the value in the first position, if that is also
-# in the sequence.
-# if both parts in sequence, part 1 index should be before part 2 index
-# A B C D if B before D and D before A then D A B C, C D A B.
-# while...if changed.
-
 incorrect_sequences = []
 for sequence in sequences:
-    # print(sequence)
+    # sequence is list[str]
     correct = True
     
     for rule in rules:
+        # rule:tuple
         if rule[0] in sequence and rule[1] in sequence:
+            # if both the first and second values are in the sequence, if the position in the sequence of the first is after the position in the 
+            # sequence of the second then that is not correct
             if sequence.index(rule[0]) > sequence.index(rule[1]):
                 correct = False
     if correct == False:
+        #if the sequence is not correctly ordered, put it in the incorrect sequence list
         incorrect_sequences.append(sequence)
-
+# incorrect sequences is list[list[str]]
 corrected_sequences = []
 for incorrect_sequence in incorrect_sequences:
     
-    # print(incorrect_sequence)
+    # incorrect sequence is list[str]
     
     changed = True
     
-
+    
     for rule in rules:
+
         to_correct_sequence = incorrect_sequence.copy()
+        # to_correct_sequence is a shallow copy of a list[str]
         while changed:
+            # end the loop unless correction sequence runs
             changed = False
+            # correction sequence runs if both parts of the rule are present in the sequence and the index for the first is larger than the index for the
+            # second
             if rule[0] in to_correct_sequence and rule[1] in to_correct_sequence:
                 
                 if to_correct_sequence.index(rule[0]) > to_correct_sequence.index(rule[1]):
+                    # if correction sequence has run, reactivate while loop
                     changed = True
+
                     first_position = to_correct_sequence.index(rule[0])
                     second_position = to_correct_sequence.index(rule[1])
+                    # this moves the second position to immediately before the first and captures the rest of the data in order
+                    # I checked this output in testing_slices.py
                     to_correct_sequence = to_correct_sequence[:second_position]+to_correct_sequence[first_position:first_position+1]+to_correct_sequence[second_position:second_position+1]+to_correct_sequence[first_position+1:]
                     
         
     corrected_sequences.append(to_correct_sequence)
-total = 0                    
+total = 0 
+# corrected_sequences:list[list[str]]                   
 for corrected_sequence in corrected_sequences:
-    middle = int(len(corrected_sequence) * 0.5)
-    total+= int(corrected_sequence[middle])
+    # corrected_sequence: list[str]
+    length_of_sequence = len(corrected_sequence)
+    
+    middle = (length_of_sequence-1)/2
+    
+    value =  corrected_sequence[int(middle)]
+    
+    total += int(value)
 
 print(total) 
+print(len(rules))
+print(sequences)
 
-# 6190 wrong
+
--- testing_slices.py
+++ testing_slices.py
@@ -1,8 +1,10 @@
-test_list = [1,2,3,4]
+test_list2 = [1,2,3,4]
+test_list = [5,6,7,8]
 first_position = 2 #3
 second_position = 1 #2
 # move 3 in front of 2
-new_list = test_list[:second_position]+test_list[first_position:first_position+1]+test_list[second_position:second_position+1]+test_list[first_position+1:]
-print(new_list)
+test_list = test_list[:second_position]+test_list[first_position:first_position+1]+test_list[second_position:second_position+1]+test_list[first_position+1:]
+# to_correct_sequence = to_correct_sequence[:second_position]+to_correct_sequence[first_position:first_position+1]+to_correct_sequence[second_position:second_position+1]+to_correct_sequence[first_position+1:]
+print(test_list)
 
 # to_correct_sequence[:first_position+1]+to_correct_sequence[second_position:second_position+1]+to_correct_sequence[second_position+1:]
